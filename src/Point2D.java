7c22200c09acc9c02b854a9508d67179cd1a25bb
thaonoucamp <dangxuanthaoht@gamil.com>
2021-01-15 17:45:37 +0700

    add

---
+++ src/Point2D.java
@@ -0,0 +1,41 @@
+public class Point2D {
+    private float x = 0.0f;
+    private float y = 0.0f;
+
+    public Point2D(float x, float y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public Point2D() {
+
+    }
+
+    public float getX() {
+        return x;
+    }
+
+    public void setX(float x) {
+        this.x = x;
+    }
+
+    public float getY() {
+        return y;
+    }
+
+    public void setY(float y) {
+        this.y = y;
+    }
+
+    public float[] getXY() {
+        float[] arr = {x, y};
+        return arr;
+    }
+
+    public String toString() {
+        return "Point2D's x: "
+                + x + "\n"
+                + "y: "
+                + y;
+    }
+}
